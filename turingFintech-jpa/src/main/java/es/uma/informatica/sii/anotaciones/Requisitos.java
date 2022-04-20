0e02e5d18348476b929c4e25b3742f9a4205586b
AlejandroR-git <0610704072@uma.es>
2022-04-20 11:41:55 +0200

    Dependencia para hacer anotaciones de requisitos añadida

---
+++ turingFintech-jpa/src/main/java/es/uma/informatica/sii/anotaciones/Requisitos.java
@@ -0,0 +1,14 @@
+package es.uma.informatica.sii.anotaciones;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface Requisitos {
+	String [] value();
+}
