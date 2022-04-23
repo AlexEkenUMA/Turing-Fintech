7227420bca286830dcb454f7d234f787606308b9
JosePan46 <josepan@uma.es>
2022-04-23 18:42:31 +0200

    carpeta para excepciones añadida y excepcion añadida

--- turingFintech-ejb/src/main/java/clases/ejb/exceptions/Prueba.java
+++
@@ -1,4 +0,0 @@
-package clases.ejb.exceptions;
-
-public class Prueba {
-}
---
+++ turingFintech-ejb/src/main/java/clases/ejb/exceptions/TuringTestException.java
@@ -0,0 +1,8 @@
+package clases.ejb.exceptions;
+
+public class TuringTestException extends Exception {
+
+    public TuringTestException(){};
+
+    public TuringTestException(String message) {super(message);}
+}
