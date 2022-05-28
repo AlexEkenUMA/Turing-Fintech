b9fd55540acb621fe75659627089b7a0a9893878
AlejandroR-git <0610704072@uma.es>
2022-05-28 17:31:19 +0200

    Endpoint Healthcheck RF11 implementado.

--- turingFintech-war/src/main/java/es/uma/turingFintech/rest/ServicioREST.java
+++ turingFintech-war/src/main/java/es/uma/turingFintech/rest/ServicioREST.java
@@ -3,9 +3,8 @@ package es.uma.turingFintech.rest;
 import clases.ejb.GestionClientes;
 import clases.ejb.GestionCuentas;
 import clases.ejb.GestionUsuarios;
-import clases.ejb.exceptions.NingunClienteCoincideConLosParametrosDeBusqueda;
-import clases.ejb.exceptions.NoEsAdministrativo;
-import clases.ejb.exceptions.UsuarioNoEncontrado;
+import clases.ejb.exceptions.*;
+import es.uma.turingFintech.Cliente;
 import es.uma.turingFintech.Usuario;
 import es.uma.turingFintech.backing.InfoSesion;
 
@@ -16,22 +15,14 @@ import javax.ejb.EJB;
 import javax.json.JsonObject;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
-@Path("/holanda")
+@Path("")
 public class ServicioREST {
     @EJB
     private GestionCuentas gestionCuentas;
@@ -40,32 +31,34 @@ public class ServicioREST {
     @EJB
     private GestionUsuarios gestionUsuarios;
 
-    private InfoSesion infoSesion;
-
     @Context
     private UriInfo uriInfo;
 
-    @Path("/producto")
+    @Path("/healthcheck")
     @GET
-    @Produces ({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response getProducto() {
-        //TODO
+    @Produces ({MediaType.APPLICATION_XML})
+    public Response healthcheck() {
         return Response.ok().build();
     }
 
-    @Path("/clientes")
+    @Path("/clients")
     @GET
-    @Produces ({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces ({MediaType.APPLICATION_JSON})
     public Response getClientes() {
         //TODO
         return Response.ok().build();
     }
 
-    @Path("/healthcheck")
-    @GET
-    @Produces ({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response healthcheck() {
+    @Path("/products")
+    @POST
+    @Produces ({MediaType.APPLICATION_JSON})
+    public Response getProducto() {
         //TODO
         return Response.ok().build();
     }
+
+
+
+
+
 }
