f3b3c28cc2e0b193754dbea97b8161709334ca67
JosePan46 <josepan@uma.es>
2022-05-29 16:29:39 +0200

    Panel de admin con estilo, vamos a la interfaz Falta: InterfazUsuario Adaptar el resto de formularios y tabalas

---
+++ turingFintech-war/src/main/java/es.uma.turingFintech.backing/FieldsetView.java
@@ -0,0 +1,18 @@
+package es.uma.turingFintech.backing;
+
+import org.primefaces.event.ToggleEvent;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class FieldsetView {
+
+    public void handleToggle(ToggleEvent event) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Toggled", "Visibility:" + event.getVisibility());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+}
